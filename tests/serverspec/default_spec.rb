46d7a1d8258c3e001858aca10726c19354cbf496
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-10-16 19:59:44 +0900

    initial implementation

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,58 +1,14 @@
 require 'spec_helper'
-require 'serverspec'
 
-package = 'su'
-service = 'su'
-config  = '/etc/su/su.conf'
-user    = 'su'
-group   = 'su'
-ports   = [ PORTS ]
-log_dir = '/var/log/su'
-db_dir  = '/var/lib/su'
+group_file = '/etc/group'
+su_users = %w[ vagrant nobody _syslogd ]
 
 case os[:family]
 when 'freebsd'
-  config = '/usr/local/etc/su.conf'
-  db_dir = '/var/db/su'
 end
 
-describe package(package) do
-  it { should be_installed }
-end 
-
-describe file(config) do
-  it { should be_file }
-  its(:content) { should match Regexp.escape('su') }
+su_users.each do |user|
 end
-
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-case os[:family]
-when 'freebsd'
-  describe file('/etc/rc.conf.d/su') do
-    it { should be_file }
-  end
-end
-
-describe service(service) do
-  it { should be_running }
-  it { should be_enabled }
-end
-
-ports.each do |p|
-  describe port(p) do
-    it { should be_listening }
-  end
+describe file(group_file) do
+  its(:content) { should match(/^wheel:\*:0:root,vagrant,nobody,_syslogd$/) }
 end
